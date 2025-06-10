2360296648c96a1e60a6f7561708aeaebaefebe0
ali <m.alikhan858@gmail.com>
2025-06-09 23:42:15 -0700

    Initial commit: Added invitation program

---
+++ invitation.py
@@ -0,0 +1,7 @@
+# Write a Python program that:
+# Takes a list of friend names.
+# Uses a for loop to send a personalized invitation message to each friend.
+
+frnd_list = ["saad", "Salman", "kamran", "sajjad"]
+for friend in frnd_list:
+    print(f"Hello Dear {friend}, I would like to invite you to my Birthday Party")
