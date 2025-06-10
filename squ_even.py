bfa0f0f2290783d83654924be2b0807578dda31a
ali <m.alikhan858@gmail.com>
2025-06-09 23:12:21 -0700

    Add square even numbers program

--- squ_even.py
+++
@@ -1,9 +0,0 @@
-# Project : Squaring Even Numbers from 1 to 10.
-
-# Using list comprehension to square even numbers
-squared_even = [num ** 2 for num in range(1, 11) if num % 2 == 0]
-
-# Print the results with better formatting
-print("Squared even numbers from 1 to 10:")
-for num, square in zip(range(2, 11, 2), squared_even):
-    print(f"{num} squared = {square}")
